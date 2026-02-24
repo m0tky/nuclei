002b58da7b8bd80797963befe35026d627d88153
Dwi Siswanto <git@dw1.io>
2026-02-24 23:32:54 +0700

    test(lib): add TestThreadSafeNucleiEngineWithNoHostErrors
    Signed-off-by: Dwi Siswanto <git@dw1.io>

--- lib/sdk_test.go
+++ lib/sdk_test.go
@@ -3,10 +3,12 @@ package nuclei_test
 import (
 	"context"
 	"log"
+	"os"
 	"testing"
 	"time"
 
 	nuclei "github.com/projectdiscovery/nuclei/v3/lib"
+	"github.com/projectdiscovery/nuclei/v3/pkg/types"
 	"github.com/stretchr/testify/require"
 )
 
@@ -54,4 +56,56 @@ func TestHeadlessOptionInitialization(t *testing.T) {
 	require.NotNil(t, ne.Logger, "logger should be initialized")
 
 	defer ne.Close()
+}
+
+func TestThreadSafeNucleiEngineWithNoHostErrors(t *testing.T) {
+	tempTemplate, err := os.CreateTemp(t.TempDir(), "thread-safe-no-host-errors.*.yaml")
+	require.NoError(t, err)
+
+	t.Cleanup(func() {
+		_ = os.Remove(tempTemplate.Name())
+	})
+
+	_, err = tempTemplate.WriteString(`id: thread-safe-no-host-errors
+info:
+  name: Thread Safe (NoHostErrors)
+  author: nuclei-sdk-test
+  severity: info
+http:
+  - method: GET
+    path:
+      - "{{BaseURL}}/"
+    matchers:
+      - type: word
+        words:
+          - "ok"`)
+	require.NoError(t, err)
+	require.NoError(t, tempTemplate.Close())
+
+	options := types.DefaultOptions().Copy()
+	options.NoHostErrors = true
+	options.Timeout = 2
+	options.Retries = 0
+	options.BulkSize = 1
+	options.TemplateThreads = 1
+	options.Templates = append(options.Templates, tempTemplate.Name())
+
+	ne, err := nuclei.NewThreadSafeNucleiEngineCtx(context.TODO(), nuclei.WithOptions(options))
+	if err != nil {
+		require.NoError(t, err, "could not create nuclei engine")
+	}
+
+	defer ne.Close()
+
+	if err := ne.GlobalLoadAllTemplates(); err != nil {
+		require.NoError(t, err, "could not load templates")
+	}
+
+	// ne.GlobalResultCallback(func(event *output.ResultEvent) {
+	// 	fmt.Println(event.Host, event.Info.SeverityHolder.Severity)
+	// })
+
+	if err := ne.ExecuteNucleiWithOptsCtx(context.TODO(), []string{"scanme.sh"}); err != nil {
+		require.NoError(t, err, "nuclei execution should not return an error")
+	}
 }
